27d68f3cfff722bbebb5aba44625cee87d1d5c92
Esteban Cortina <estebannsebastian@gmail.com>
2023-04-20 08:10:13 -0600

    POST

--- App/server.js
+++ App/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 function f1(n) {
   if (n === 0 || n === 1) {
@@ -18,6 +20,15 @@ app.get('/factorial/:value', (req, res) => {
   let number = req.params;
 
   res.json({ resultado: f1(number.value) });
+})
+
+app.get('/productos/', (req, res) => {
+  res.send("<h1>Productos</h1>");
+})
+
+app.post('/productos', (req, res) => {
+  console.log(req.body);
+  res.json({ msg: "Creado" });
 })
 
 app.listen(3000)
